9552fbe771a16e57c0e4bf15e7653513a41b10ff
Pankaj <Pankaj@Mascot>
2015-09-05 17:03:37 +0530

    added new fom for signup

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -17,48 +17,31 @@ if(!isset($login_model)) {
     $login_model = new \common\models\LoginForm();
 }
 ?>
-
-<div class="content container" id="main-content">
-    <div class="row">
-        <div id="main-content-container">
-            <div class="h1_top_border"></div>
-            <h1><?=$title?></h1>
-            <div class="h1_bottom_border"></div>
-
-            <div class="row">
-                <div class="col-sm-12">
-                    <h3 style="margin-bottom: 0px;"><?=Yii::t('frontend','Intro...')?></h3>
-                    
-                </div>
-            </div>
-            
-            <div class="row">
-                <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
-                    <div class="col-sm-6">                    
-                        <?php if ($signup_model->errors != null && sizeof($signup_model->errors)>0) {
-                            echo '<p class="help-block-error">' . Yii::t('app','Input error.') . "</p>";
-                        } ?>
-                        
-                        <?= $form->field($signup_model, 'name') ?>
-                        <?= $form->field($signup_model, 'email') ?>
-                        <?= $form->field($signup_model, 'password')->passwordInput() ?>
-                        <?= $form->field($signup_model, 'password_repeat')->passwordInput() ?>
-                        
-                    </div>
-                    <div class="col-sm-6"> 
-                        
-                        <p><?=Yii::t('app','Los campos marcados con <strong>*</strong> son obligatorios.')?></p>
-
-                        <div class="form-group">
-                            <?= Html::submitButton(Yii::t('app','Signup'), ['class' => 'btn boton registrame', 'name' => 'signup-button']) ?>
-                        </div>
-                    </div>
-                
-                <?php ActiveForm::end(); ?>
-                
-
-
-            </div>
-        </div>
+<section class="eRegistration">
+	<div class="boxo">
+            <?php $form = ActiveForm::begin(['id' => 'form-login']); ?>
+    	<p class="bigtitle"><?=Yii::t('frontend','Login')?></p>
+    	<input type="text" value="" placeholder="_mail" onfocus="this.placeholder = ''" onblur="this.placeholder = '_mail'"  />
+        <div class="clear"></div>
+        <input type="text" value="" placeholder="_contrasena" onfocus="this.placeholder = ''" onblur="this.placeholder = '_password'" />
+        <button class="smlsbtn">Login</button>
+        <div class="clear"></div>
+         <?php ActiveForm::end(); ?>
+        <a href="#" class="linkz">I forgot my password</a>
     </div>
-</div>
+    <div class="boxo">
+         <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+    	<p class="bigtitle"><?=Yii::t('frontend','New User? Sign up')?></p>
+    	 <?= $form->field($signup_model, 'name') ?>
+        <div class="clear"></div>
+        <?= $form->field($signup_model, 'email') ?>
+		<div class="clear"></div>
+        <?= $form->field($signup_model, 'password')->passwordInput() ?>
+	<div class="clear"></div>
+        <?= $form->field($signup_model, 'password_repeat')->passwordInput() ?>
+        <?= Html::submitButton(Yii::t('app','Signup'), ['class' => 'smlsbtn', 'name' => 'signup-button']) ?>
+        <div class="clear"></div>
+         <?php ActiveForm::end(); ?>
+                
+    </div>
+</section>
