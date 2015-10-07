6a32d8772da55e128224f7b6117ef6db05de97c2
Diego <Diego@192.168.1.33>
2015-10-07 22:11:13 +0200

    Show/hide map and srcolls

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -15,7 +15,7 @@ AppAsset::register($this);
 <html>
 <head>
 <meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1" />
-<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+<link rel="icon" href="<?=Yii::getAlias("@web");?>/images/favicon.ico" type="image/x-icon">
 
 <!--<title>Planeta Verde | Passionate about nature</title>-->
 
@@ -29,7 +29,7 @@ AppAsset::register($this);
 <!--## css file ##-->
 <link type="text/css" rel="stylesheet"  href="<?=Yii::getAlias("@web")?>/css/reset.css" media="all" />
 <link type="text/css" rel="stylesheet"  href="<?=Yii::getAlias("@web")?>/css/style.css" media="screen" />
-<!--<link rel='stylesheet prefetch' href='css/bootstrap.min.css'>-->
+<link rel='stylesheet prefetch' href='css/bootstrap.min.css'>
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <link type="text/css" rel="stylesheet" href="<?=Yii::getAlias("@web")?>/css/style.min.css" />
 
@@ -55,7 +55,7 @@ AppAsset::register($this);
     </ul>
 
     <ul class="login">
-        <li><a href="#"><img src="<?=Yii::getAlias("@web")?>/images/wishlist.png" alt="" /></a></li>
+        <li><a href="<?=  Url::to(["/Wishlist"]); ?> "><img src="<?=Yii::getAlias("@web")?>/images/wishlist.png" alt="" /></a></li>
         <li><a href="#" class="oddz"><input type="text" placeholder="search" onfocus="this.placeholder = ''" onblur="this.placeholder = 'search'" /><img src="<?=Yii::getAlias("@web")?>/images/search.png" alt="" /></a></li>
         <li><a target="blank" href="<?= backend\modules\settings\models\Settings::getParamValue("front-page", "facebook-link");?> "><img src="<?=Yii::getAlias("@web")?>/images/facebook.png" alt="" /></a></li>
         
@@ -64,8 +64,8 @@ AppAsset::register($this);
                 if(Yii::$app->user->isGuest) { ?>
                     <a href="<?= yii\helpers\Url::to(["site/signup"]); ?>"><img src="<?=Yii::getAlias("@web")?>/images/login.png" alt="" /></a>
                 <?php } else { ?>
-                    <a  style="float:right" href="<?= Url::to(["site/logout"]); ?>" data-method="post"><img src="<?=Yii::getAlias("@web")?>/images/logout.png" alt="logout" /></a>
-                    <a href="<?= Url::to(["/user/index"]); ?>" class="goto-user-link"><?=Yii::$app->user->identity->email;?></a>
+                    <a data-method="post" style="float:right" href="<?= Url::to(["/logout"]); ?>" data-method="post"><img src="<?=Yii::getAlias("@web")?>/images/logout.png" alt="logout" /></a>
+                    <a href="<?= Url::to(["/user-profile/index"]); ?>" class="goto-user-link"><?=Yii::$app->user->identity->email;?></a>
                     <a  style="float:right"><img src="<?=Yii::getAlias("@web")?>/images/user.png" alt="" /></a>
                 <?php } ?>
                         
@@ -75,48 +75,6 @@ AppAsset::register($this);
     
     <?= $content; ?>
     
-    <!--
-    <div class="spoggle">
-        <a class="softArrow" href="#">
-            <img src="<?=Yii::getAlias("@web")?>/images/softArrowRight.png" alt="" />
-        </a>
-        <div class="spogglez"> 
-            <div class="mapBox">
-                <a href="#"><img class="mapthis" src="<?=Yii::getAlias("@web")?>/images/mapToggle.jpg" alt="" /></a>
-            </div>
-            <div class="contentBox">
-                <p class="heading1">How to use</p>
-                <p class="heading4">Texto explicativo del uso del mapa.</p>
-            </div>
-        </div>
-    </div>
-    -->
-    
-    <!--
-    <section class="filosofia">
-	<div id='mycustomscroll' class='flexcroll'>
-            <p class="bigtitle1">Filosofia</p>
-            <p>Wir sind ein für Tierbeobachtung spezialisiertes Reiseunternehmen, das bezweckt, qualitativ hochwertige Naturreisen anzubieten.</p>
-            <p>Die Authentizität der letzten großen Tierparadiese unseres Planeten näher zu bringen, ist was, uns zur Gründung dieses Unternehmens bewegte. Unser Ziel dabei: Reisen zu konzipieren, die eine lebensverändernde Wirkung auf Gäste haben.</p>
-            <p>Dies mag etwas gewagt sein, aber wir wissen, dass es möglich ist. Und wir wissen auch, dass uns dieses Objektiv bei jeder
-            neuen Reise mehr annähren.</p>
-            <p>In diesen Seiten wollen wir Sie Hinter den Kulissen von Planeta Verde führen, damit Sie lernen, wer wir sind, was wir machen, wie
-            wir denken und alles, was Sie bei uns erwarten können und das, was Sie nicht erwarten können. Wir hoffen, dass Sie am Ende
-            der Lektüre besser wissen, ob wir die Veranstalter sind, denen Sie die Planung Ihrer Traumnaturreise anvertrauen wollen oder, ob
-            Sie sich an jemanden anderen lieber wenden.</p>
-            <p>Es erübrigt sich zu sagen, dass eine Truppe Tier- und Reiseliebhaber hinter Planeta Verde steckt. Uns verbindet eine gemeinsame Leidenschaft für Reisen in die Natur und die ständige Sorge, welchen bescheidenen Beitrag wir zu einer nachhaltigen</p>
-            <p>Ein schneller Überblick unseres Reiseangebotes genügt, um zu merken, dass wir überwiegend Entwicklungsländer ins Programm aufgenommen haben. Die Gründe dafür liegen auf der Hand: Hier präsentiert sich die Natur häufig am Spektakulärsten.</p>
-            <p>Aber auch hier ist diese am akutsten gefährdet. Für die meisten dieser Länder bedeuten die Erträge aus dem Ökotourismus die
-            einzigen Mitteln, eine halbweg effektive Naturschutzpolitik auf die Beine zu stellen. Der Weg zum Bestehend bleiben und Ausdehnen der Naturschutzgebiete führt jedoch meistens an einer erzielten Verstärkung der Besucherzahlen nicht vorbei. Dies ist</p>
-            <p>klar gut für die Wirtschaft. Aber was gut für die Wirtschaft ist, muss nicht unbedingt gut für die lokale Fauna sein. Die Lebensbedingungen der Tierwelt und die Qualität der Tierbeobachtung leiden immer mehr unter einer außer Kontrolle geratenen Politik</p>
-            <p>der „je mehr Touristen, desto besser“. Dazu beigetragen hat die relativ neue geratene Mode des Ökotourismus mit seiner manchmal leichtsinnigen Klientel.
-            Frühere unberührte Tierparadiese gleichen heutzutage einem bunten Zoo, wo man Jeeps in gleichen Zahlen wie wilde Tiere
-            beobachten kann. Man denkt allein zum Beispiel an den Ngorongoro-Krater oder an die Nebelwälder Costa Ricas, die täglich von
-            lauten Touristenscharen erobert werden.</p>
-        </div>
-    </section>
-    -->
-
 </section>
 <!--  mobile menu  -->
 <nav id="c-menu--slide-left" class="c-menu c-menu--slide-left">
