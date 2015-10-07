604be78d014e7dd864bda7fe2ebb7fa141b31cbc
Diego <Diego@192.168.1.33>
2015-10-07 22:17:23 +0200

    users sign up

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -4,26 +4,33 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=planetaverde_db',
+            'dsn' => 'mysql:host=localhost;dbname=planetaverde_db_zhixiang',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
+            'useFileTransport' => true,
+            'fileTransportPath' => "C://mail"
+        ],
+        /*
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            
             'viewPath' => '@common/mail',
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => false,
+            'useFileTransport' => true,
             'transport' => [
                 'class' => 'Swift_SmtpTransport',
                 'host' => 'smtp.gmail.com',
-                'username' => 'demo.narola@gmail.com',
-                'password' => 'Ke6g7sE70Orq3Rqaqa',
+                'username' => '',
+                'password' => '',
                 'port' => '465',
                 'encryption' => 'ssl',
             ],
-        ],
+        ],*/
     ],
 ];
